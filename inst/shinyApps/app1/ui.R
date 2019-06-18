1b835483d7515fbaf49f6a9f5fbf84efa8438b2f
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-06-18 13:40:20 +0200

    no need for subscription

--- inst/shinyApps/app1/ui.R
+++ inst/shinyApps/app1/ui.R
@@ -57,22 +57,7 @@ ui <- tagList(
         #     )
         #   )
         # )
-      ),
-    tabPanel("Abonnement"
-      # ,
-      # sidebarLayout(
-      #   sidebarPanel(width = 3,
-      #     selectInput("subscriptionRep", "Rapport:", c("Samlerapport1", "Samlerapport2")),
-      #     selectInput("subscriptionFreq", "Frekvens:",
-      #                 list(Årlig="year", Kvartalsvis="quarter", Månedlig="month", Ukentlig="week", Daglig="DSTday"),
-      #                 selected = "month"),
-      #     actionButton("subscribe", "Bestill!")
-      #   ),
-      #   mainPanel(
-      #     uiOutput("subscriptionContent")
-      #   )
-      # )
-    )
+      )
 
   ) # navbarPage
 ) # tagList
